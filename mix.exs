aed7a43c65d8120455a0c8db3e64d10a3f183ab2
Debora Martins <deh.vmartins@gmail.com>
2022-09-16 16:24:06 -0300

    downgrade lib

--- mix.exs
+++ mix.exs
@@ -37,7 +37,7 @@ defmodule OpenTelemetryDecorator.MixProject do
       {:ex_doc, "~> 0.25.3", only: :dev, runtime: false},
       {:excoveralls, "~> 0.14.0", only: :test, runtime: false},
       {:opentelemetry, "~> 1.1.0", only: :test},
-      {:opentelemetry_api, "~> 1.1.0"},
+      {:opentelemetry_api, "~> 1.0.3"},
       {:opentelemetry_exporter, "~> 1.1.0", only: :test}
     ]
   end
